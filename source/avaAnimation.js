d19dc88cc7db2179fcf47fedb61b3d22fa25a762
yKlym <yaroslaw.klymko@gmail.com>
2020-05-03 16:02:57 +0300

    avatar animation added

--- source/avaAnimation.js
+++ source/avaAnimation.js
@@ -1,6 +1,15 @@
-(function(){
-// ava animations will be here
+(function () {
+    const ava = $("img.avatar");
 
+    $("img.avatar").mouseover((event)=>{
+        console.log(event.target);
+        $("img.avatar").addClass("shake");
+    });
+
+    $("img.avatar").mouseout((event)=>{
+        console.log(event.target);
+        $("img.avatar").removeClass("shake");
+    });
 
 
 })();
