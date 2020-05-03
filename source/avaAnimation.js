08bc1d847fd7c47d17bb50759164026705410d06
yKlym <yaroslaw.klymko@gmail.com>
2020-05-03 16:05:47 +0300

    fast clg fix

--- source/avaAnimation.js
+++ source/avaAnimation.js
@@ -2,12 +2,10 @@
     const ava = $("img.avatar");
 
     $("img.avatar").mouseover((event)=>{
-        console.log(event.target);
         $("img.avatar").addClass("shake");
     });
 
     $("img.avatar").mouseout((event)=>{
-        console.log(event.target);
         $("img.avatar").removeClass("shake");
     });
 
